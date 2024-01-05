465ed891e4e1dcdc640568696fd068b8baa0435c
johncornelles <john.cornellews@kalvium.community>
2024-01-05 17:23:41 +0530

    Update carousel component with new styles and functionality.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import React, { Component } from "react";
 import Carousel from "./components/Carousel";
 import "./App.css";
-
-
+import {images} from "./data/CarouselData.jsx";
 
 export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Carousel />
-      </div>
+        <Carousel images={images} />
     );
   }
 }
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,13 +1,40 @@
 import React, { Component } from "react";
 import "./Carousel.css";
-import { images } from "../data/CarouselData";
-// you can explore more - and check as how to use materiul ui
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
-// implement the class below
 class Carousel extends Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      currImage: 0
+    };
+  }
+
+  prev = () => {
+    let images = this.props.images
+    let newIndex = ((this.state.currImage - 1 + images.length) % images.length)
+    this.setState({currImage: newIndex})
+  }
+  next = () => {
+    let images = this.props.images
+    let newIndex = (this.state.currImage + 1) % images.length
+    this.setState({currImage: newIndex})
+  }
+  render() {
+    const images  = this.props.images;
+    return (
+      <div className="carousel">
+      <button onClick={this.prev}><ArrowBackIosIcon/></button>
+        <div className="datas">
+          <h1>{images[this.state.currImage].title}</h1>
+          <p>{images[this.state.currImage].subtitle}</p>
+          <img src={images[this.state.currImage].img} alt="" />
+        </div>
+      <button onClick={this.next}><ArrowForwardIosIcon/></button>
+      </div>
+    );
+  }
 }
 
 export default Carousel;
